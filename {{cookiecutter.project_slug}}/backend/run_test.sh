9d16e7adf02ad8190f4cf046e68288c1265ac882
Ilya Beda <ir4y.ix@gmail.com>
2020-09-03 11:23:02 +0300

    Fix pipenv launch

--- {{cookiecutter.project_slug}}/backend/run_test.sh
+++ {{cookiecutter.project_slug}}/backend/run_test.sh
@@ -14,7 +14,7 @@ if [ -z "${TESTS_AIDBOX_LICENSE_ID}" ]; then
     exit 1
 fi
 
-export TEST_COMMAND=" wait-for-it.sh devbox:8080 --strict --timeout=0 -- pytest --junitxml=report.xml --cov-report html --cov-report term:skip-covered --doctest-modules --cov app ${@:-tests/}"
+export TEST_COMMAND=" wait-for-it.sh devbox:8080 --strict --timeout=0 -- pipenv run pytest --junitxml=report.xml --cov-report html --cov-report term:skip-covered --doctest-modules --cov app ${@:-tests/}"
 
 docker-compose -f docker-compose.tests.yaml up --exit-code-from backend backend
 exit $?
