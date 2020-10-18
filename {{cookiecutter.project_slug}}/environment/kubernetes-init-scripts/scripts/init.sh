71635dab0fdd396fb1448cfaef04a34929db1094
Vadim Laletin <ruscoder@users.noreply.github.com>
2020-10-18 10:08:48 +0300

    Ref #2 Add SECRET_KEY to secrets

--- {{cookiecutter.project_slug}}/environment/kubernetes-init-scripts/scripts/init.sh
+++ {{cookiecutter.project_slug}}/environment/kubernetes-init-scripts/scripts/init.sh
@@ -8,6 +8,8 @@ AIDBOX_CLIENT_SECRET=`pwgen 16 1`
 AIDBOX_ADMIN_PASSWORD=`pwgen 16 1`
 PGPASSWORD=`pwgen 16 1`
 APP_SECRET=`pwgen 16 1`
+SECRET_KEY=`pwgen 32 1`
+
 KEY_OPENID_RSA=`base64 jwtRS256.key -w 0`
 KEY_OPENID_RSA_PUB=`base64 jwtRS256.key.pub -w 0`
 EOF
