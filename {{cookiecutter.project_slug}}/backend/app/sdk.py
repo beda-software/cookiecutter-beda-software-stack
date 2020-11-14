750dd9e651e30850f801365b173321fe5f7dd289
Ilya Beda <ir4y.ix@gmail.com>
2020-11-14 19:29:47 +0300

    Update sdk.py

--- {{cookiecutter.project_slug}}/backend/app/sdk.py
+++ {{cookiecutter.project_slug}}/backend/app/sdk.py
@@ -1,8 +1,10 @@
-from aiohttp import web
 from aidbox_python_sdk.sdk import SDK
 from aidbox_python_sdk.settings import Settings
 
+from aiohttp import web
+
 from app.manifest import entities, meta_resources, seeds, migrations
+
 
 sdk_settings = Settings(**{})
 sdk = SDK(
@@ -13,10 +15,9 @@ sdk = SDK(
     migrations=migrations,
 )
 
+
 @sdk.operation(
-    ["GET"],
-    ["healthcheck"],
-    public=True,
+    ["GET"], ["healthcheck"], public=True,
 )
 async def healthcheck(operation, request):
-    return web.json_response({'is_ready': sdk.is_ready.result()})
+    return web.json_response({"is_ready": sdk.is_ready.result()})
