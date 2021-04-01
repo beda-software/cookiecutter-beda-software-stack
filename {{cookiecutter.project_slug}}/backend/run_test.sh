cf6ef55d34c15436e9b9688f42590754e0eed488
Vadim Laletin <ruscoder@users.noreply.github.com>
2021-04-01 17:14:07 +0300

    Update run_test.sh

--- {{cookiecutter.project_slug}}/backend/run_test.sh
+++ {{cookiecutter.project_slug}}/backend/run_test.sh
@@ -15,6 +15,6 @@ if [ -z "${TESTS_AIDBOX_LICENSE_ID}" ]; then
 fi
 
 export TEST_COMMAND=" wait-for-it.sh devbox:8080 --strict --timeout=0 -- pipenv run pytest --junitxml=report.xml --cov-report html --cov-report term:skip-covered --doctest-modules --cov app ${@:-tests/}"
-
+docker-compose -f docker-compose.tests.yaml up --exit-code-from dockerize dockerize || exit 1
 docker-compose -f docker-compose.tests.yaml up --exit-code-from backend backend
 exit $?
